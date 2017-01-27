4d633e2ee219d6835089d99d4be9c8bd39562e13
Russ Miles <russ@atomist.com>
2017-01-27 12:23:49 +0000

    Added initial executor to apply editor across multiple projects
    Contributes to: #15

---
+++ .atomist/executors/SetSpringBootParentVersionExecutor.ts
@@ -0,0 +1,31 @@
+import { Executor } from "@atomist/rug/operations/Executor"
+import { Services, Service } from "@atomist/rug/model/Core"
+import { Result, Status, Parameter } from "@atomist/rug/operations/RugOperation"
+
+let params: Parameter[] = [
+    {
+        name: "spring_boot_version",
+        displayName: "Spring Boot Version",
+        description: "version of Spring Boot, e.g., 1.4.4.RELEASE",
+        validInput: "a valid version of Spring Boot",
+        pattern: "@any",
+        required: false
+    }
+]
+
+interface Parameters {
+    spring_boot_version: string
+}
+
+var setSpringBootParentVersionExecutor: Executor = {
+    description: "Set the Spring Boot version on the parent block in any pom in a project",
+    name: "SetSpringBootParentVersion",
+    tags: ["atomist/intent=set spring boot parent version", "atomist/private=false"],
+    parameters: params,
+    execute(services: Services, p:Parameters): Result {
+      for (let s of services.services()) {
+        s.editWith("SetSpringBootParentVersion", {spring_boot_version: p.spring_boot_version})
+      }
+      return new Result(Status.Success, "OK")
+    }
+}
